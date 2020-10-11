772e2cb3322680fe75ea58cc0bec5ed73f34ea8f
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-11 19:25:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,11 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  planeteer_calls.map {|element| element.capitalize}
+  def first 
+    planeteer_calls.map {|element| element.capitalize}
+  end
+  array = first
+  first.map {|fe| fe + "!"}
 end
 
 def long_planeteer_calls(planeteer_calls)
